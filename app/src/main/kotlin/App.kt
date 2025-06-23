a1755fe4e03002e7395108fb14179fca4b57fb62
Natchanon Lapo <nattanonnon2018@gmail.com>
2025-06-23 12:20:36 +0700

    first commit

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,17 @@
+fun main() {
+    Kop()
+
+}
+fun Kop(){
+    var Kint: Int = 20_000
+    var Kboo: Boolean = true
+    var Kstr: String = "class24"
+    var Kdo: Double = 200.01
+    var KA: Any = "class"
+    println("$Kint \n $Kboo \n $Kstr \n $Kdo \n $KA")
+    println("---------------------------------------")
+    var a: Double = 2.0
+    var b: Double = 4.0
+    println(a%b)
+
+}
